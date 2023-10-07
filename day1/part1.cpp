bfc542fec56bcfd58122e719298be0589907695e
MakuAureo <makuaureo@gmail.com>
2023-10-07 15:32:35 -0300

    Fixed an oversight with part1.cpp

--- day1/part1.cpp
+++ day1/part1.cpp
@@ -7,7 +7,7 @@ int main() {
   std::ifstream input("fileInput.txt");
   std::string string;
   int currentSum = 0;
-  int biggestElement;
+  int biggestElement = 0;
 
   //Get input size
   input.seekg(0, input.end);
@@ -19,8 +19,11 @@ int main() {
     std::getline(input, string);
     //Get sum
     if (string.length()) { currentSum += std::stoi(string); }
-    //Update biggestElement
-    else if (!string.length() && currentSum > biggestElement) { biggestElement = currentSum; }
+    //Update currentSum and biggestElement
+    else if (!string.length()) {
+      if (currentSum > biggestElement) biggestElement = currentSum;
+      currentSum = 0;
+    }
   }
 
   //Print biggestElement
