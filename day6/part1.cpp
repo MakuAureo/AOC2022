328938894c012ea0ae2179ebcad8e17707d648e0
MakuAureo <makuaureo@gmail.com>
2023-11-17 23:52:46 -0300

    Added day6

---
+++ day6/part1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_set>
+
+///Note to self:
+///  while this solution is short (very short in fact) I'm not sure
+///  it is a good one, I have no experience with sets and as such
+///  have no ideia how efficient they are in code
+
+int main() {
+  //Initial declaration
+  std::ifstream input("fileInput.txt");
+  std::unordered_set<char> strSet;
+  std::string inputString;
+  int markerSize = 4;
+
+  //Extract input string
+  std::getline(input, inputString);
+
+  //Iterate over the string
+  int pos = 0;
+  for (; pos < inputString.size() - markerSize; pos++) {
+    //Looking at 4 characters at a time
+    std::string substr = inputString.substr(pos, markerSize);
+
+    //Pass them to a set
+    strSet.insert(substr.begin(), substr.end());
+    //If the has size == markerSize then the substr must be
+    //all different characters
+    if (strSet.size() == markerSize) { break; }
+    else { strSet.clear(); }
+  }
+
+  //Output result
+  std::cout << pos + markerSize << std::endl;
+
+  return 0;
+}
